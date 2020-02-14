7ec1e19d05a4d4c3cf5201d0617a520e5ad82d84
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-14 00:14:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,12 @@ def monopoly_with_third_tier
        :two_pieces_owned => 50,
        :three_pieces_owned => 100,
        :four_pieces_owned => 200 },
-     :names => {:a => {} ,:b => {},:c => {},:d => {} }
+     :names => 
+     {:a => {} ,
+     :b => {},
+     :c => {},
+     :d => {} 
+     }
     }
   }
 end
