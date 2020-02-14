75c8154872a48f0d3cc21a2c1b2abb4dc7c2967b
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-14 00:05:09 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,10 +18,11 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   {:railroads => 
-    {:pieces => 4, :rent_in_dollars => 
-      {:a => {} },:b => {},:c => {},:d => {} }
-    },
-    {:names => {}}
+    {:pieces => 4, 
+     :rent_in_dollars => 
+      {:a => {} },:b => {},:c => {},:d => {} },
+    :names => {}
+    }
   }
 end
 
