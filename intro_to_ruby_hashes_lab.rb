6766d843cb9733024d1d3871a285cab9e3d06bbb
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-14 00:20:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,7 +25,7 @@ def monopoly_with_third_tier
        :three_pieces_owned => 100,
        :four_pieces_owned => 200 },
      :names => 
-     {:reading_railroad => {} ,
+     {:reading_railroad => {"mortgage_value"=>"$100"} ,
      :pennsylvania_railroad => {} ,
      :b_and_o_railroad => {} ,
      :shortline_railroad => {}  
