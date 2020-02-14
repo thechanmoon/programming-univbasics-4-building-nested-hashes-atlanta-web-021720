fd0619a5b85b0593a0be20bf090a40fa17176b1c
CHANMOON PARK <thechanmoon@gmail.com>
2020-02-14 00:24:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -39,7 +39,7 @@ def monopoly_with_fourth_tier
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
-    {:railroads => 
+  {:railroads => 
     {:pieces => 4, 
      :rent_in_dollars => 
       {:one_piece_owned => 25 ,
